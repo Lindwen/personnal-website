b3a566905275eb4aea8004abaea9f4135e191c43
Lindwen <commit@lindwen.fr>
2024-07-07 16:54:30 +0200

    chore: Update main.js with Konami code functionality

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -12,3 +12,43 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   });
 });
+
+console.log(
+  "%cSTOP! ✋ This area is intended for developers.",
+  "font-size: 18px; font-weight: 700; color: red;"
+);
+const konamiCode = [
+  "ArrowUp",
+  "ArrowUp",
+  "ArrowDown",
+  "ArrowDown",
+  "ArrowLeft",
+  "ArrowRight",
+  "ArrowLeft",
+  "ArrowRight",
+  "b",
+  "a",
+];
+
+let konamiCodePosition = 0;
+
+function checkKonamiCode(event) {
+  const key = event.key;
+
+  const requiredKey = konamiCode[konamiCodePosition];
+
+  if (key === requiredKey) {
+    konamiCodePosition++;
+
+    if (konamiCodePosition === konamiCode.length) {
+      alert("コナミコマンド");
+      window.open("https://www.youtube.com/watch?v=dQw4w9WgXcQ", "_blank");
+
+      konamiCodePosition = 0;
+    }
+  } else {
+    konamiCodePosition = 0;
+  }
+}
+
+document.addEventListener("keydown", checkKonamiCode);
